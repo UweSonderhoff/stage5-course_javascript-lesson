f4482b104741c4b0a4a2c0da6abe97f165a6eb01
UweSonderhoff <uwe.sonderhoff@me.com>
2015-11-12 16:12:38 +0100

    Commit: Lession Quiz Content
    Everything until String manipulation Part 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,9 +1,34 @@
+// Console.log function test + defining variables for my name and role.
+var name = "Uwe Sonderhoff";
+console.log(name);
+
+var role = "Product Manager";
+
+
 /*
-This is empty on purpose! Your code to build the resume will go here.
- */
+Replace method used with two different variables where a string is being replaced 
+and the string is being rendered in html by selecting and CSS ID
+*/
+var awesomeThoughts = "I am Uwe and I am AWESOME";
+console.log(awesomeThoughts);
 
- var name = "james";
- console.log(name);
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+console.log(funThoughts);
 
- var awesomeThoughts = "I am Uwe and I am AWESOME";
- console.log(awesomeThoughts);
+//COMMENTED OUT: $("#main").append(funThoughts);
+
+
+// Replacing method used with set email variable
+var email = "uwe.sonderhoff@lecturio.de";
+var NewEmail =
+    email.replace("lecturio.de", "me.com");
+console.log(email);
+console.log(NewEmail);
+
+
+// Combine .replace and .append/prepend between 2 js files and therewith adding content to a page by CSS ID
+var formattedName = HTMLheaderName.replace("%data%", name);
+$("#header").prepend(formattedName);
+
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").append(formattedRole);
