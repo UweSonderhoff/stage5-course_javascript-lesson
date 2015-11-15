e99795ccba5d623e5dd7424762b9cab05fff19d4
UweSonderhoff <uwe.sonderhoff@me.com>
2015-11-15 20:48:27 +0100

    Commit: All quizzes done in JavaScript Lesson
    All the things in the JavaScript Lession are done and code is in this
    file

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,22 +1,21 @@
+/* COMMENTED OUT - not used anymore
 // Console.log function test + defining variables for my name and role.
-/* not used anymore
 var name = "Uwe Sonderhoff";
 console.log(name);
 
 var role = "Product Manager";
-*/
 
-/*
-Replace method used with two different variables where a string is being replaced
-and the string is being rendered in html by selecting and CSS ID
-*/
+
+
+// Replace method used with two different variables where a string is being replaced
+// and the string is being rendered in html by selecting and CSS ID
 var awesomeThoughts = "I am Uwe and I am AWESOME";
 console.log(awesomeThoughts);
 
 var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
 console.log(funThoughts);
 
-//COMMENTED OUT: $("#main").append(funThoughts);
+//$("#main").append(funThoughts);
 
 
 // Replacing method used with set email variable
@@ -25,19 +24,93 @@ var NewEmail =
     email.replace("lecturio.de", "me.com");
 console.log(email);
 console.log(NewEmail);
+*/
 
 
-// Playing with JavaScript objects
 
+// JavaScript objects: Creating Objects with different options
+
+// --- Literal notation ---
 var bio = {
   "name" : "Uwe Sonderhoff",
   "role" : "Product Manager",
-  "contact-info" : "Leipzig",
-  "picture-url" : "url",
+  "contacts" : "Germany"
+  "location" : "Leipzig",
   "welcomeMsg" : "Welcome Message",
-  "skills" : "Many"
+  "skills" : ["Skill 1", "Skill 2"]
 }
 
+/* COMMENTED OUT
+// --- Dot notation ---
+var work = {}
+work.workTitle = "Head of Product";
+work.description = "Develop online product"
+work.employer = "My Company";
+work.yearsWorked = 5;
+work.workLocation = "Leipzig";
+
+// --- Bracket notation ---
+var education = {}
+education["schoolName"] = "GAB Gymnasium";
+education["schoolYears"] = 9;
+education["schoolLocation"] = "Benndorf";
+*/
+
+// --- Nested objects with literal notation ---
+var education = {
+    "schools": [
+      {
+        "type": "elementary school",
+        "city": "Siebigerode",
+        "majors": "none",
+        "minors": "none",
+        "graduationYears": 4,
+        "onlineCourseInformation": "What should that be?"
+      },
+      {
+        "type": "primary school",
+        "city": "Benndorf",
+        "majors": ["math", "english language"],
+        "minors": ["economics", "german language"],
+        "graduationYears": 9,
+        "onlineCourseInformation": "What should that be?"
+      }
+    ]
+}
+
+var projects = {
+    "project" : [
+        {
+            "title" : "Travel24.com Launch",
+            "description" : "Launch and Development of Travel24.com",
+            "date" : "2010-2012",
+            "images" : ["no image", "no image"]
+        },
+        {
+            "title" : "Apion",
+            "description" : "Develop Mobile Apps in Android and iOS",
+            "date" : "2012-2015"
+            "images" : ["no image", "no image"]
+        }
+    ]
+}
+
+var work = {
+    "jobs" : [
+        {
+            "workTitle" = "Head of Product";
+            "description" = "Develop online product"
+            "employer" = "My Company";
+            "yearsWorked" = 5;
+            "workLocation" = "Leipzig";
+        }
+    ]
+}
+
+
+
+
+/* COMMENTED OUT
 // Combine .replace and .append/prepend between 2 js files and therewith adding content to a page by CSS ID
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 $("#header").prepend(formattedName);
@@ -45,5 +118,12 @@ $("#header").prepend(formattedName);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 $("#header").append(formattedRole);
 
-var formattedwelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
-$("#main").append(formattedwelcomeMsg);
+var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
+$("#main").append(formattedWelcomeMsg);
+
+var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.workTitle);
+$("#main").append(formattedWorkTitle);
+
+var formattedSchoolName = HTMLschoolName.replace("%data%", education.schoolName);
+$("#main").append(formattedSchoolName);
+*/
